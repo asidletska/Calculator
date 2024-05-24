1edf18e2a6e0fca9bb2b85d1b608f829437dd375
WIN-FI93QU5E7B9\Asidletska <nastiasidletskaya@gmail.com>
2024-05-24 15:24:35 +0300

    Create repository

---
+++ Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine(" Enter an expression ");
+
+                string input = Console.ReadLine();
+                string[] data = input.Split(' ');
+                string operation = data[1];
+                int x = int.Parse(data[0]), y = int.Parse(data[2]), result = 0;
+
+                switch (operation) 
+                {
+                    case "+":
+                        result = x + y;
+                    break;
+                    case "-":
+                        result = x - y;
+                    break;
+                    case "*":
+                        result = x * y;
+                    break;
+                    case "/":
+                        result = x / y;
+                    break;
+                }
+                Console.WriteLine(" Result:" + result);
+            }
+        }
+    }
